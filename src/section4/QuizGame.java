e355e32e3dcbfc0d77c66544dff85b4bc7c26304
root <root@8463ddd4c86c>
2020-06-25 19:01:11 +0000

    Section 4

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,50 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
-		// 1.  Create a variable to hold the user's score 
-		
-		// 2.  Ask the user a question 
-		
-		// 3.  Use an if statement to check if their answer is correct
-		
-		// 4.  if the user's answer was correct, add one to their score 
-		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
-		// 6.  After all the questions have been asked, print the user's score 
-		
+
+		int quizScore = 0;
+		String userAnswer1 = JOptionPane.showInputDialog("What is the capital of Canada?");
+		if ((userAnswer1.equalsIgnoreCase("ottawa"))) {
+			quizScore+=1;
+			JOptionPane.showMessageDialog(null, "You were correct! Your score is now " + quizScore + "!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "You were wrong. Your score is still " + quizScore + "!");
+		}
+		String userAnswer2 = JOptionPane.showInputDialog("What is the largest country in the world?");
+		if ((userAnswer2.equalsIgnoreCase("russia"))) {
+			quizScore+=1;
+			JOptionPane.showMessageDialog(null, "You were correct! Your score is now " + quizScore + "!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "You were wrong. Your score is still " + quizScore + "!");
+		}
+		String userAnswer3 = JOptionPane.showInputDialog("What is the largest river in the world?");
+		if ((userAnswer3.equalsIgnoreCase("amazon river"))) {
+			quizScore+=1;
+			JOptionPane.showMessageDialog(null, "You were correct! Your score is now " + quizScore + "!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "You were wrong. Your score is still " + quizScore + "!");
+		}
+		String userAnswer4 = JOptionPane.showInputDialog("What is the smallest country in the world?");
+		if ((userAnswer4.equalsIgnoreCase("Vatican City"))) {
+			quizScore+=1;
+			JOptionPane.showMessageDialog(null, "You were correct! Your score is now " + quizScore + "!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "You were wrong. Your score is still " + quizScore + "!");
+		}
+		if (quizScore > 3) {
+			JOptionPane.showMessageDialog(null, "Wow! You got all the questions correct! Congratulations!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Good job! You got a score of " + quizScore + "/4! Can you do better next time?");
+		}
 	}
 }
